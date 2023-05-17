7302d4d066009a0f054c70a6f5a0acbd24bbd5f8
Ayush Bhandarkar <95070060+ayushbhandarkar@users.noreply.github.com>
2023-05-17 19:34:24 +0530

    Time: 855 ms (78.34%), Space: 47.3 MB (59.88%) - LeetHub

---
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+  def pairSum(self, head: Optional[ListNode]) -> int:
+    def reverseList(head: ListNode) -> ListNode:
+      prev = None
+      while head:
+        next = head.next
+        head.next = prev
+        prev = head
+        head = next
+      return prev
+
+    ans = 0
+    slow = head
+    fast = head
+
+    # Let slow point to the start of the second half
+    while fast and fast.next:
+      slow = slow.next
+      fast = fast.next.next
+
+    # Tail points to the end with reversed second half
+    tail = reverseList(slow)
+
+    while tail:
+      ans = max(ans, head.val + tail.val)
+      head = head.next
+      tail = tail.next
+
+    return ans
